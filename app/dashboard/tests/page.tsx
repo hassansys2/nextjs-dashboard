a52f3dea3510191ca59781ad8e92bf50968ad36e
Hassan Ahmad <h.ahmed@arhamsoft.org>
2024-06-27 17:40:44 +0500

    Added Trigger Page

--- app/dashboard/tests/page.tsx
+++ app/dashboard/tests/page.tsx
@@ -12,7 +12,10 @@
   }
   ```
 */
-export default function Example() {
+
+import { lusitana } from '@/app/ui/fonts';
+
+export default async function Page() {
   return (
     <>
       {/*
@@ -25,55 +28,30 @@ export default function Example() {
       */}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=orange&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+          <h2 className={`${lusitana.className} mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-orange-600`}>
+            Trigger Pentest
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-orange-600 hover:text-orange-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="mt-2 flex space-x-4">
+              <input
+                id="target"
+                name="target"
+                type="url"
+                placeholder="http://www.target.com or IP"
+                required
+                className="flex-grow rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+              />
+              <input
+                id="port"
+                name="port"
+                type="number"
+                placeholder="0-1000"
+                required
+                className="w-20 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+              />
             </div>
 
             <div>
@@ -81,15 +59,15 @@ export default function Example() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
               >
-                Sign in
+                Attack
               </button>
             </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
+            need customized scan ?{' '}
             <a href="#" className="font-semibold leading-6 text-orange-600 hover:text-orange-500">
-              Start a 14 day free trial
+              Advance Scan
             </a>
           </p>
         </div>
